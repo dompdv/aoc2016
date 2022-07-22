8261a39714d2309012168938886935e1cb7ead38
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-07-22 23:17:50 +0200

    Day3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,27 @@
 defmodule AdventOfCode.Day03 do
+  import Enum
+
+  def check_triangle([a, b, c]), do: a + b > c and b + c > a and a + c > b
+
   def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> map(fn c -> String.split(c, " ", trim: true) |> map(&String.to_integer/1) end)
+    |> filter(&check_triangle/1)
+    |> count()
   end
 
   def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> map(fn c -> String.split(c, " ", trim: true) |> map(&String.to_integer/1) end)
+    |> chunk_every(3)
+    |> map(fn [[a, b, c], [d, e, f], [g, h, i]] -> [[a, d, g], [b, e, h], [c, f, i]] end)
+    |> map(fn l -> map(l, &check_triangle/1) end)
+    |> List.flatten()
+    |> frequencies()
+    |> Map.get(true)
+
+    #   |> map(fn t1, t2, t3 -> )
   end
 end
--- lib/mix/tasks/d03.p2.ex
+++ lib/mix/tasks/d03.p2.ex
@@ -5,7 +5,16 @@ defmodule Mix.Tasks.D03.P2 do
 
   @shortdoc "Day 03 Part 2"
   def run(args) do
-    input = nil
+    input = """
+    101 301 501
+    102 302 502
+    103 303 503
+    201 401 601
+    202 402 602
+    203 403 603
+    """
+
+    input = AdventOfCode.Input.get!(3, 2016)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
