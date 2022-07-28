92812cd3167f65407610e77093d61ad2b31a871e
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-07-28 22:19:48 +0200

    Day15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Day15 do
-  def part1(args) do
+  import Enum
+
+  def parse(args) do
+    reduce(
+      String.split(args, "\n", trim: true),
+      %{},
+      fn line, acc ->
+        [_, disc, total_pos, pos] =
+          Regex.run(
+            ~r/Disc #([0-9]+) has ([0-9]+) positions; at time=0, it is at position ([0-9]+)./,
+            line
+          )
+
+        Map.put(acc, String.to_integer(disc), %{
+          total: String.to_integer(total_pos),
+          start_pos: String.to_integer(pos)
+        })
+      end
+    )
   end
 
+  def check_time(discs, t) do
+    all?(
+      for {disc, %{total: total, start_pos: start_pos}} <- discs,
+          do: rem(start_pos + t + disc, total) == 0
+    )
+  end
+
+  def find_right_time(discs) do
+    Stream.iterate(0, &(&1 + 1))
+    |> Stream.drop_while(fn i -> not check_time(discs, i) end)
+    |> take(1)
+  end
+
+  def part1(args), do: find_right_time(parse(args))
+
   def part2(args) do
+    discs = parse(args)
+
+    Map.put(discs, count(discs) + 1, %{total: 11, start_pos: 0})
+    |> find_right_time()
   end
 end
--- lib/mix/tasks/d15.p1.ex
+++ lib/mix/tasks/d15.p1.ex
@@ -5,7 +5,12 @@ defmodule Mix.Tasks.D15.P1 do
 
   @shortdoc "Day 15 Part 1"
   def run(args) do
-    input = nil
+    input = """
+    Disc #1 has 5 positions; at time=0, it is at position 4.
+    Disc #2 has 2 positions; at time=0, it is at position 1.
+    """
+
+    input = AdventOfCode.Input.get!(15, 2016)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
